a0aabf31a500566aaa1e8887df66955ea769322d
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-17 06:58:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 // Write your code here
+let num1 = 32;
+let num2 = 2;
+let num3 = '';
 
 let multiply = num1 * num2;
 
 let random = Math.random();
+
+let mod = num3/num4
+
+let max = Number.max(1, 4, 6, 20);
